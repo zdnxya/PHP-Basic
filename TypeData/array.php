e03579beb9ffe1adce52cfbf029eb3bb12e36554
Zidan <zidanhadiprakoso@gmail.com>
2025-06-12 14:45:31 +0700

    added array

---
+++ TypeData/array.php
@@ -0,0 +1,22 @@
+<?php 
+$arr = array(1,2,3,4,5,16);
+
+echo $arr[0] = "test" . "\n";
+
+echo count($arr) . "\n";
+
+// dictionary
+
+$dict = array(
+    "name" => "z",
+    "age" => 17,
+    "status" => "unknown"
+);
+echo $dict['name'];
+
+
+
+
+
+
+?>
