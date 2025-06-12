db377d0ba3cb86c21e54807c3bc3b48c39346b12
Zidan <zidanhadiprakoso@gmail.com>
2025-06-12 11:15:55 +0700

    added TypeData

---
+++ TypeData/boolean.php
@@ -0,0 +1,8 @@
+<?php 
+echo "True/On :";
+var_dump(true);
+
+echo "False/Off :";
+var_dump(false);
+
+?>
---
+++ TypeData/num.php
@@ -0,0 +1,18 @@
+<?php 
+echo "Binary(base 2) : ";
+var_dump(0b111);
+
+echo "Octal(base 8) : ";
+var_dump(010);
+
+echo "Decimal(base 10) : ";
+var_dump(12345);
+
+echo "Hexadecima(base 16) : ";
+var_dump(0x12345);
+
+// Float
+echo "Float Number : ";
+var_dump(12.000);
+
+?>
