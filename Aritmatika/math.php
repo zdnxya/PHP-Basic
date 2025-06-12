d8283e488f158d160b4975aa45930affe1495e9b
Zidan <zidanhadiprakoso@gmail.com>
2025-06-12 15:20:44 +0700

    added math folder

---
+++ Aritmatika/math.php
@@ -0,0 +1,49 @@
+<?php
+
+function addition($x, $y){
+    $result = $x + $y;
+    echo "addition : $result \n";
+}
+
+function subtraction($x, $y){
+    $result = $x - $y;
+    echo "subtraction : $result \n";
+}
+
+function multiplication($x, $y){
+    $result = $x * $y;
+    echo "mutliplication : $result \n";
+}
+
+function division($x, $y){
+    $result = $x / $y;
+    echo "Division : $result". "\n";
+}
+
+addition(20,30);
+subtraction(20,30);
+multiplication(20,2);
+division(20,2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
