1edf07753cba51506ab2dbf524c4a5dfffadd0b1
Zidan <zidanhadiprakoso@gmail.com>
2025-06-24 12:32:50 +0700

    added manipulate string

---
+++ ManipString/string.php
@@ -0,0 +1,15 @@
+<?php 
+$name = "puki";
+$age = 17;
+$bio = "wsp hoe";
+
+echo "Name : " . $name . " Age : " . $age . PHP_EOL;
+echo $bio . PHP_EOL;
+
+// ganti type data
+
+$stringValue = (int)"100";
+var_dump($stringValue);
+
+
+?>
